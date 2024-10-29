41a77323df7987facd21a3f07fafcb5ca50798b4
Woolf3105 <ElectraBris@gmail.com>
2024-10-29 19:36:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ print(name)
 age = 36
 print ('Name:', age)
 new_age = age + 12
-print("New age: ", new_age)
+print('New age: ', new_age)
 is_student = True
-print("Is student: ",is_student)
+print('Is student: ',is_student)
